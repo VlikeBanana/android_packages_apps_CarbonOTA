2035c8f4563277dec8db8ab88703be3db16292e4
VlikeBanana <AndrolinRoot@googlemail.com>
2013-07-24 17:46:20 +0200

    XYE Android.mk prepare

--- Android.mk
+++ Android.mk
@@ -6,7 +6,7 @@ LOCAL_SRC_FILES += $(call all-java-files-under, src)
 LOCAL_STATIC_JAVA_LIBRARIES := \
     android-support-v4_13
  
-LOCAL_PACKAGE_NAME := CarbonOTA
+LOCAL_PACKAGE_NAME := XYEOTA
 LOCAL_CERTIFICATE := platform
  
 include $(BUILD_PACKAGE)
